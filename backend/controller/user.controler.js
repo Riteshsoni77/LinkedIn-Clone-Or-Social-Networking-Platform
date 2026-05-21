f731f49448dd752a73768fe67c2cf494b22ebb7a
Riteshsoni77 <riteshsoni1138@gmail.com>
2026-05-22 03:16:44 +0530

    add a updateprofilepicture controller

--- backend/controller/user.controler.js
+++ backend/controller/user.controler.js
@@ -1,68 +1,87 @@
 
-import User from"../models/usermodel.js";
+import User from "../models/usermodel.js";
 import bcrypt from 'bcrypt';
 import Profile from "../models/profilemodel.js";
 import crypto from 'crypto';
 import { TokenOutlined } from "@mui/icons-material";
 
-export const register= async ( req, res )=>{
+export const register = async (req, res) => {
 
-    try{
-        const {name, email,password, username }=req.body;
+    try {
+        const { name, email, password, username } = req.body;
 
-        if (!name|| !email || !password||!username) return res.status(400).json({message:"all fields are required"});
+        if (!name || !email || !password || !username) return res.status(400).json({ message: "all fields are required" });
 
-        const user =await User.findOne({
+        const user = await User.findOne({
             email
         });
-        if (user)return res.status (400).json({message:"user alrady exist"});
-   
-        const  hashedPassword=await bcrypt.hash(password, 10);
-        const  newUser=new User({
+        if (user) return res.status(400).json({ message: "user alrady exist" });
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
             name,
             email,
-            password:hashedPassword,
+            password: hashedPassword,
             username
         });
-      await newUser.save();
+        await newUser.save();
 
-      const profile=new Profile({userId:newUser._id  })
+        const profile = new Profile({ userId: newUser._id })
 
-      return res.json({message:"User Created "});
+        return res.json({ message: "User Created " });
 
-    }catch(error){
-        return res.status(500).json({message:error.message})
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
     }
-   
+
 }
 
 
-export const login= async( req, res)=>{
+export const login = async (req, res) => {
 
-    try{
-        const { email,password}=req.body;
-         if ( !email || !password) return res.status(400).json({message:"all fields are required"});
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: "all fields are required" });
 
-        const user =await User.findOne({
+        const user = await User.findOne({
             email
         });
-        if (!user)return res.status (404).json({message:"user not exist"});
-       const ismatch=await bcrypt.compare(password,user.password);
+        if (!user) return res.status(404).json({ message: "user not exist" });
+        const ismatch = await bcrypt.compare(password, user.password);
 
-       if (!ismatch ) return res.status(400).json({message:"Invalid password"});
+        if (!ismatch) return res.status(400).json({ message: "Invalid password" });
 
-       const token=crypto.randomBytes(32).toString("hex");
-       
-       await User.updateOne({_id:user._id},{token});
+        const token = crypto.randomBytes(32).toString("hex");
 
-       return res.json({message:"Login Succesful",token});
+        await User.updateOne({ _id: user._id }, { token });
+
+        return res.json({ message: "Login Succesful", token });
 
 
 
-    }catch(error){
+    } catch (error) {
 
     }
-    
-   
+
+
+
+}
+
+export const uploadprofilepicture = async (req, res) => {
+    const { token } = req.body;
+    try {
+
+        const user = await User.findOne({ token: token });
+        if (!user) {
+            return res.Status(400).json({ message: "user not found" });
+        }
+        user.profilePicture = req.file.filename;
+        await user.save();
+        return res.json({ message: "Profile Picture Updated" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
 
 }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,8 +1,21 @@
-
 import {Router} from "express";
-import { login, register } from "../controller/user.controler.js";
+import { login, register,uploadprofilepicture } from "../controller/user.controler.js";
+
+import multer from "multer";
 const router=Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+router.route("/update_profile_picture").post(upload.single('profile_picture'),uploadprofilepicture)
 router.route('/register').post(register);
 router.route('/login').post(login);
 
