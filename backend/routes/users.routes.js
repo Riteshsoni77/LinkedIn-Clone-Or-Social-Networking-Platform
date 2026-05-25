431effa109706a8fcfcfdff6272b3a1a468354d8
Riteshsoni77 <riteshsoni1138@gmail.com>
2026-05-26 00:45:47 +0530

    add conection routes and  the controllers

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,6 +1,10 @@
 import {Router} from "express";
 import { login, register,uploadprofilepicture,updateUserProfile,getUserAndProfile,
-  updateProfileData, getAllUserProfile,downloadProfile} from "../controller/user.controler.js";
+  updateProfileData, getAllUserProfile,downloadProfile,
+  sendConnectionRequest,
+  getMyConnectionReqests,
+  whatAreMyConnections,
+  acceptConnectionRequest} from "../controller/user.controler.js";
 
 import multer from "multer";
 const router=Router();
@@ -24,7 +28,10 @@ router.route('/get_user_and_profile').get(getUserAndProfile);
 router.route('/update_profile_data').post(  updateProfileData);
 router.route('/user/get_all_users').get(getAllUserProfile);
 router.route('/user/download_resume').get(downloadProfile);
-// router.route('/user/send/connection_request')
+ router.route('/user/send/connection_request').post(sendConnectionRequest);
+ router.route('user/getConnectionrequests').get(getMyConnectionReqests);
+ router.route('user/user_connection_request').get(whatAreMyConnections);
+ router.route('user/accept_connection_request').post(acceptConnectionRequest);
 
 
 export default router;
