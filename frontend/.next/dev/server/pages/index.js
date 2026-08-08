d0ec879065470f71bfb3e0b9e0d8ea8bf516c209
Riteshsoni77 <riteshsoni1138@gmail.com>
2026-08-09 01:59:58 +0530

    update

--- frontend/.next/dev/server/pages/index.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/node_modules_0scy8eh._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__095qme.._.js")
-R.c("server/chunks/ssr/node_modules_129w7gn._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0t8l382._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__02jes_9._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sfE7RqhDDDyjvlsytwt5VZQuvNolI/mNE0Hhrhqj7iE=\"\n}"
