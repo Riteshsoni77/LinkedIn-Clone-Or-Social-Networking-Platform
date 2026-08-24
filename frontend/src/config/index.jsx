aecd288234629d83e799b59cce6e9e7fd6ae49e2
Riteshsoni77 <riteshsoni1138@gmail.com>
2026-08-25 00:08:26 +0530

    some update for the deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 const { default:axios}=require("axios");
 
    // export const  BASE_URL="http://localhost:9000";  
-    export const  BASE_URL=" https://linkedin-clone-or-social-networking.onrender.com";
+    export const  BASE_URL="https://linkedin-clone-or-social-networking.onrender.com";
 
    
 
